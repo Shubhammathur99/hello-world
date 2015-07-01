2d98602df74260f639d0e3693cbba70bdcc43a99
Shubhammathur99 <Shubham.mathur99@gmail.com>
2015-07-01 14:02:54 -0700

    Create curser.c
    this function is for creating basic curser skills.

---
+++ curser.c
@@ -0,0 +1,34 @@
+;--------------------------------------------;
+    ;current  curser  location;
+    ;formula= X+Y*col;
+;--------------------------------------------;
+BITS 32
+movcurser:
+pusha
+xor eax,eax
+mov ecx,cols
+mov al,bl
+mul ecx
+add al,bl
+mov ebx,eax
+;--------------------------------------;
+	;   Set low byte index to VGA register ;
+	;--------------------------------------;
+	mov al,0x0f
+	mov dx,03D4
+	out dx,al
+	mov al,bl
+	mov dx,03D4
+	out dx,al
+	;--------------------------------------;
+	;   Set high byte index to VGA register ;
+	;--------------------------------------;
+ mov al,0x0e
+ mov dx,03D4
+ mov dx,al
+ mov al,bh
+ mov dx,03D4
+ mov dx,al
+ popa
+ ret
+    
